5b96a207c98b9395ff248f57b520e03104f4020d
yorickburgers <yorickburgers@live.nl>
2024-12-05 15:25:55 +0100

    map, button, get request & state done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css';
+import axios from 'axios'
+import {useState} from "react";
 
 function App() {
 
+    const [countryInfo, setCountryInfo] = useState([])
+
+
+    async function fetchCountryInfo() {
+        try {
+            const result = await axios.get('https://restcountries.com/v3.1/all')
+        console.log(result);
+            setCountryInfo(result.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    console.log(countryInfo);
+
     return (
         <>
-            Maak je applicatie hier!
+            <span className="imageWrapper">
+                <img src="../src/assets/world_map.png" alt="iamge of world map"/>
+            </span>
+            <button type="button" className="mapButton" onClick={fetchCountryInfo}>Haal landinformatie op!</button>
         </>
     )
 }
